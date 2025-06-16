1edaffc2ce4e0579c9e91a594a2e7794e9902080
WitoldFa <wito9876@wp.pl>
2025-06-16 10:22:40 +0200

    Rotate

--- Lab4/Form1.Designer.cs
+++ Lab4/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             pictureBox1 = new PictureBox();
             btnLoad = new Button();
+            radio90 = new RadioButton();
+            radio180 = new RadioButton();
+            radio270 = new RadioButton();
+            btnRotate = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -51,22 +55,78 @@
             btnLoad.UseVisualStyleBackColor = true;
             btnLoad.Click += btnLoad_Click;
             // 
+            // radio90
+            // 
+            radio90.AutoSize = true;
+            radio90.BackColor = Color.FromArgb(255, 128, 128);
+            radio90.Location = new Point(169, 37);
+            radio90.Name = "radio90";
+            radio90.Size = new Size(46, 24);
+            radio90.TabIndex = 2;
+            radio90.TabStop = true;
+            radio90.Text = "90";
+            radio90.UseVisualStyleBackColor = false;
+            // 
+            // radio180
+            // 
+            radio180.AutoSize = true;
+            radio180.BackColor = Color.FromArgb(255, 128, 128);
+            radio180.Location = new Point(169, 67);
+            radio180.Name = "radio180";
+            radio180.Size = new Size(54, 24);
+            radio180.TabIndex = 3;
+            radio180.TabStop = true;
+            radio180.Text = "180";
+            radio180.UseVisualStyleBackColor = false;
+            // 
+            // radio270
+            // 
+            radio270.AutoSize = true;
+            radio270.BackColor = Color.FromArgb(255, 128, 128);
+            radio270.Location = new Point(169, 94);
+            radio270.Name = "radio270";
+            radio270.Size = new Size(54, 24);
+            radio270.TabIndex = 4;
+            radio270.TabStop = true;
+            radio270.Text = "270";
+            radio270.UseVisualStyleBackColor = false;
+            // 
+            // btnRotate
+            // 
+            btnRotate.BackColor = Color.FromArgb(255, 128, 128);
+            btnRotate.Location = new Point(164, 124);
+            btnRotate.Name = "btnRotate";
+            btnRotate.Size = new Size(94, 29);
+            btnRotate.TabIndex = 5;
+            btnRotate.Text = "Rotate";
+            btnRotate.UseVisualStyleBackColor = false;
+            btnRotate.Click += btnRotate_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnRotate);
+            Controls.Add(radio270);
+            Controls.Add(radio180);
+            Controls.Add(radio90);
             Controls.Add(btnLoad);
             Controls.Add(pictureBox1);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private PictureBox pictureBox1;
         private Button btnLoad;
+        private RadioButton radio90;
+        private RadioButton radio180;
+        private RadioButton radio270;
+        private Button btnRotate;
     }
 }
--- Lab4/Form1.cs
+++ Lab4/Form1.cs
@@ -3,6 +3,30 @@ namespace Lab4
     public partial class Form1 : Form
     {
         private Bitmap originalImage;
+
+        private Bitmap RotateImage(Bitmap source, float angle)
+        {
+            float radian = angle * (float)Math.PI / 180f;
+            float cos = Math.Abs((float)Math.Cos(radian));
+            float sin = Math.Abs((float)Math.Sin(radian));
+            int newWidth = (int)(source.Width * cos + source.Height * sin);
+            int newHeight = (int)(source.Width * sin + source.Height * cos);
+
+            Bitmap rotated = new Bitmap(newWidth, newHeight);
+            rotated.SetResolution(source.HorizontalResolution, source.VerticalResolution);
+
+            using (Graphics g = Graphics.FromImage(rotated))
+            {
+                g.TranslateTransform(newWidth / 2f, newHeight / 2f);
+                g.RotateTransform(angle);
+                g.TranslateTransform(-source.Width / 2f, -source.Height / 2f);
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                g.DrawImage(source, new Point(0, 0));
+            }
+
+            return rotated;
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +43,27 @@ namespace Lab4
                 originalImage = new Bitmap(ofd.FileName);
                 pictureBox1.Image = originalImage;
             }
+        }
+
+        private void btnRotate_Click(object sender, EventArgs e)
+        {
+            if (originalImage == null)
+            {
+                MessageBox.Show("Najpierw wczytaj obraz.");
+                return;
+            }
+
+            float angle = 0;
+            if (radio90.Checked) angle = 90;
+            else if (radio180.Checked) angle = 180;
+            else if (radio270.Checked) angle = 270;
+            else
+            {
+                MessageBox.Show("Wybierz k?t obrotu.");
+                return;
+            }
+
+            pictureBox1.Image = RotateImage(originalImage, angle);
         }
     }
 }
